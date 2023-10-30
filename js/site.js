9a5254d9d78ca23f31226d06d89c733a2ad4105d
mariamabarry <barry.hadja16@gmail.com>
2023-10-29 21:19:33 -0400

    built out the app
    - redesigned Rewind logo
    - built App page
    - embedded Lot font

--- js/site.js
+++ js/site.js
@@ -1,3 +1,36 @@
-function HelloWorld(){
-    alert("hello world!");
+
+/*CONTROLLER FUNCTION */
+//get the values from the Page
+function getInput()
+{
+    //get user input
+    let inputStr = document.getElementById("inputStr").value;
+    
+    //reverse user input
+    let outputStr = reverseStringMethod(inputStr);
+
+    //display results
+    displayReversed(outputStr);
+}
+
+
+/*LOGIC FUNCTIONS*/
+//generate numbers from startValue to endValue
+function reverseStringMethod (str) {
+    return str.split('').reduce((reversed, character) => character + reversed, '')
+  }
+  
+  function reverseStringArr(str) {
+    let reversed = "";
+    const l = str.length;
+    for(let i = l-1; i >= 0; i--) {
+      reversed = `${reversed}${str[i]}`;
+    }
+    return reversed;
+  }
+/* DISPLAY OR VIEW FUNCTIONS*/
+//display the numbers with the even numbers being bold
+function displayReversed(str){
+
+    document.getElementById("outputStr").innerHTML = str;
 }
