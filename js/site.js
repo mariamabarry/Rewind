873c229008c0c38d83d3346b7595f497268bd742
mariamabarry <barry.hadja16@gmail.com>
2023-11-09 13:02:10 -0500

    App completed
    Built out all pages of the app
    Changed the logo design

--- js/site.js
+++ js/site.js
@@ -3,34 +3,58 @@
 //get the values from the Page
 function getInput()
 {
+    document.getElementById("alert").classList.add("invisible");
+
     //get user input
     let inputStr = document.getElementById("inputStr").value;
     
-    //reverse user input
-    let outputStr = reverseStringMethod(inputStr);
-
-    //display results
-    displayReversed(outputStr);
+    //Validate input 
+    if(!inputStr)
+    {
+      //display results
+      displayString("", false);
+    }
+    else
+    {
+      //reverse user input
+      let outputStr = reverseString(inputStr);
+      //display results
+      displayString(outputStr);
+    }
 }
 
-
 /*LOGIC FUNCTIONS*/
-//generate numbers from startValue to endValue
-function reverseStringMethod (str) {
+//reverse input string
+function reverseString (str) 
+{
     return str.split('').reduce((reversed, character) => character + reversed, '')
+}
+
+/* DISPLAY OR VIEW FUNCTIONS*/
+//display the reversed string
+function displayString(revStr, isReversible = true)
+{
+  //clear styling on alert box
+  document.getElementById("alert").classList.remove("alert-success", "alert-danger", "alert-warning", "alert-info");
+
+  if(isReversible)
+  {
+    //write to page
+    document.getElementById("alertHeading").innerHTML = `Success!`;
+    document.getElementById("msg").innerHTML = `<strong>Your string reversed is: </strong><lead>${revStr}</lead>`;
+    //style alert box
+    document.getElementById("alert").classList.add("alert-success");
+  }
+  else
+  {
+    //write to page
+    document.getElementById("alertHeading").innerHTML = `Error`;
+    document.getElementById("msg").innerHTML = `Nothing to reverse! Please enter a string.`;
+    //style alert box
+    document.getElementById("alert").classList.add("alert-danger");
   }
   
-  function reverseStringArr(str) {
-    let reversed = "";
-    const l = str.length;
-    for(let i = l-1; i >= 0; i--) {
-      reversed = `${reversed}${str[i]}`;
-    }
-    return reversed;
-  }
-/* DISPLAY OR VIEW FUNCTIONS*/
-//display the numbers with the even numbers being bold
-function displayReversed(str){
-
-    document.getElementById("outputStr").innerHTML = str;
+  //show the alert box
+  document.getElementById("alert").classList.remove("invisible");
+  document.getElementById("alert").classList.add("show");
 }
